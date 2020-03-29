3da3e773d779c1fec903cf91bf432383bd69151f
吴迪玮 <wudiwei@vv.cn>
2020-03-29 09:13:22 +0800

    f**k ReactNative, bye!!!!

--- HomeMain.js
+++ HomeMain.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+const BANNER_IMGS = [
+  require('./images/banner/bankList_whiteCard_bg.png'),
+  require('./images/banner/couponcenter_category_bg.png'),
+  require('./images/banner/bankList_whiteCard_bg.png'),
+  require('./images/banner/couponcenter_category_bg.png'),
+];
 
 class HomeMain extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _renderPage(data) {
+    return (
+      <Image source={data} style={styles.page}/>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
-        <Text style={{color:'red'}} > Main </Text>
+        <ScrollView 
+          horizontal={true} 
+          pagingEnabled={true}
+          // bounces={false}
+          style={styles.viewPager}  >
+          {this._renderPage(BANNER_IMGS[0])}
+          {this._renderPage(BANNER_IMGS[1])}
+          {this._renderPage(BANNER_IMGS[2])}
+          {this._renderPage(BANNER_IMGS[3])}
+        </ScrollView>
       </View>
     );
   }
@@ -37,6 +41,19 @@ const styles = StyleSheet.create({
     height:1200,
     width:'100%',
     backgroundColor:'black'
+  },
+  viewPager: {
+    height: 130,
+    width: '100%',
+    flexDirection: 'column',
+  },
+  pagerContent: {
+    
+  },
+  page: {
+    width: '100%',
+    height: 130,
+    resizeMode: 'cover'
   }
 });
 
